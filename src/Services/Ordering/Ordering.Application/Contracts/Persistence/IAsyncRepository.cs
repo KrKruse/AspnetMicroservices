20c89853766ae8e60029355d932f1bda77b5727b
Kristoffer Nørlund Kruse <kris69j1@edu.ucl.dk>
2021-12-02 05:20:12 +0100

    Created folders for application layer and created contracts/interfaces and mapping

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Ordering.Domain.Common;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+	public interface IAsyncRepository<T> where T : EntityBase
+	{
+		// get alt metode async
+		Task<IReadOnlyList<T>> GetAllAsync();
+		// get metode, der tager parameters (kan filtrer hvad vi vil have frem) 
+		Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
+		// samme måde som - forventer orderby 
+		Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+										Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+										string includeString = null,
+										bool disableTracking = true);
+		// kan bruge includes 
+		Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+									   Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+									   List<Expression<Func<T, object>>> includes = null,
+									   bool disableTracking = true);
+		// get ud fra id
+		Task<T> GetByIdAsync(int id);
+		// sidste 3 er for crud - opdater, opret og slet (standard)
+		Task<T> AddAsync(T entity);
+		Task UpdateAsync(T entity);
+		Task DeleteAsync(T entity);
+	}
+}
---
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+        // get order fra username/bruger - Hele mappen Persistence er til at arbejde med database relaterede ting 
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
+    }
+}
