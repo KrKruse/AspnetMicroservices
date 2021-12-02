5cd9549e0315f8647f8b018c7508971ccd7d30f2
Kristoffer Nørlund Kruse <kris69j1@edu.ucl.dk>
2021-12-02 07:02:53 +0100

    first feauture - getorderlist by name and mapping with dto in getorder (OrderVm)

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    // vil blive trickered igennem dependency injektion 
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        // contructer, får c# til at lave den for mig
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        // fået odrene ved at sende en request med username 
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -5,13 +5,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 
 namespace Ordering.Application.Mappings
 {
     public class MappingProfile : Profile
     {
+        // adgang til mapping i gennem f.eks getorderlistqueri  
         public MappingProfile()
         {
+            CreateMap<Order, OrdersVm>().ReverseMap();
         }
     }
 }
