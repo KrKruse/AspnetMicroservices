81693f8e143418eb5e2af0e8542520b2552f36d1
Kristoffer Nørlund Kruse <kris69j1@edu.ucl.dk>
2021-12-03 16:01:01 +0100

    Made controller and baskerrepository injektion in startup

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Basket.API.Repositories;
 
 namespace Basket.API
 {
@@ -25,11 +26,15 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // connect database igennem port nr(port nr eer i json filen) - database er lavet med docker og terminal
+            // connect database igennem port nr(port nr eer i json filen) - database er lavet med docker og terminal og Rediscahe(en slags pakke/json fil)
             services.AddStackExchangeRedisCache(options =>
             {
                 options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
+
+            // injection af IBasketRepository - hvis programmet ser IBasketRepository vil den lave object igennem basketrepository 
+            services.AddScoped<IBasketRepository, BasketRepository>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
